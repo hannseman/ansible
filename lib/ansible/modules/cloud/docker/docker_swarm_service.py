17cf76f12d2169415e8fd5a5e35d2d9049870cdb
Hannes Ljungberg <hannes@5monkeys.se>
2019-01-08 11:03:59 +0100

    Indent docker module documentation properly

--- lib/ansible/modules/cloud/docker/docker_swarm_service.py
+++ lib/ansible/modules/cloud/docker/docker_swarm_service.py
@@ -21,268 +21,267 @@ options:
   name:
     required: true
     description:
-    - Service name
+      - Service name
   image:
     required: true
     description:
-    - Service image path and tag.
-      Maps docker service IMAGE parameter.
+      - Service image path and tag. Maps docker service IMAGE parameter.
   state:
     required: true
     default: present
     description:
-    - Service state.
+      - Service state.
     choices:
-    - present
-    - absent
+      - present
+      - absent
   args:
     required: false
     default: []
     description:
-    - List comprised of the command and the arguments to be run inside
-    - the container
+      - List comprised of the command and the arguments to be run inside
+      - the container
   constraints:
     required: false
     default: []
     description:
-    - List of the service constraints.
-    - Maps docker service --constraint option.
+      - List of the service constraints.
+      - Maps docker service --constraint option.
   hostname:
     required: false
     default: ""
     description:
-    - Container hostname
-    - Maps docker service --hostname option.
-    - Requires api_version >= 1.25
+      - Container hostname
+      - Maps docker service --hostname option.
+      - Requires api_version >= 1.25
   tty:
     required: false
     type: bool
     default: False
     description:
-    - Allocate a pseudo-TTY
-    - Maps docker service --tty option.
-    - Requires api_version >= 1.25
+      - Allocate a pseudo-TTY
+      - Maps docker service --tty option.
+      - Requires api_version >= 1.25
   dns:
     required: false
     default: []
     description:
-    - List of custom DNS servers.
-    - Maps docker service --dns option.
-    - Requires api_version >= 1.25
+      - List of custom DNS servers.
+      - Maps docker service --dns option.
+      - Requires api_version >= 1.25
   dns_search:
     required: false
     default: []
     description:
-    - List of custom DNS search domains.
-    - Maps docker service --dns-search option.
-    - Requires api_version >= 1.25
+      - List of custom DNS search domains.
+      - Maps docker service --dns-search option.
+      - Requires api_version >= 1.25
   dns_options:
     required: false
     default: []
     description:
-    - List of custom DNS options.
-    - Maps docker service --dns-option option.
-    - Requires api_version >= 1.25
+      - List of custom DNS options.
+      - Maps docker service --dns-option option.
+      - Requires api_version >= 1.25
   force_update:
     required: false
     type: bool
     default: False
     description:
-    - Force update even if no changes require it.
-    - Maps to docker service update --force option.
-    - Requires api_version >= 1.25
+      - Force update even if no changes require it.
+      - Maps to docker service update --force option.
+      - Requires api_version >= 1.25
   labels:
     required: false
     description:
-    - List of the service labels.
-    - Maps docker service --label option.
+      - List of the service labels.
+      - Maps docker service --label option.
   container_labels:
     required: false
     description:
-    - List of the service containers labels.
-    - Maps docker service --container-label option.
+      - List of the service containers labels.
+      - Maps docker service --container-label option.
     default: []
   endpoint_mode:
     required: false
     description:
-    - Service endpoint mode.
-    - Maps docker service --endpoint-mode option.
+      - Service endpoint mode.
+      - Maps docker service --endpoint-mode option.
     default: vip
     choices:
-    - vip
-    - dnsrr
+      - vip
+      - dnsrr
   env:
     required: false
     default: []
     description:
-    - List of the service environment variables.
-    - Maps docker service --env option.
+      - List of the service environment variables.
+      - Maps docker service --env option.
   log_driver:
     required: false
     default: json-file
     description:
-    - Configure the logging driver for a service
+      - Configure the logging driver for a service
   log_driver_options:
     required: false
     default: []
     description:
-    - Options for service logging driver
+      - Options for service logging driver
   limit_cpu:
     required: false
     default: 0.000
     description:
-    - Service CPU limit. 0 equals no limit.
-    - Maps docker service --limit-cpu option.
+      - Service CPU limit. 0 equals no limit.
+      - Maps docker service --limit-cpu option.
   reserve_cpu:
     required: false
     default: 0.000
     description:
-    - Service CPU reservation. 0 equals no reservation.
-    - Maps docker service --reserve-cpu option.
+     - Service CPU reservation. 0 equals no reservation.
+      - Maps docker service --reserve-cpu option.
   limit_memory:
     required: false
     default: 0
     description:
-    - Service memory limit in MB. 0 equals no limit.
-    - Maps docker service --limit-memory option.
+      - Service memory limit in MB. 0 equals no limit.
+      - Maps docker service --limit-memory option.
   reserve_memory:
     required: false
     default: 0
     description:
-    - Service memory reservation in MB. 0 equals no reservation.
-    - Maps docker service --reserve-memory option.
+      - Service memory reservation in MB. 0 equals no reservation.
+      - Maps docker service --reserve-memory option.
   mode:
     required: false
     default: replicated
     description:
-    - Service replication mode.
-    - Maps docker service --mode option.
+      - Service replication mode.
+      - Maps docker service --mode option.
   mounts:
     required: false
     description:
-    - List of dictionaries describing the service mounts.
-    - Every item must be a dictionary exposing the keys source, target, type (defaults to 'bind'), readonly (defaults to false)
-    - Maps docker service --mount option.
+      - List of dictionaries describing the service mounts.
+      - Every item must be a dictionary exposing the keys source, target, type (defaults to 'bind'), readonly (defaults to false)
+      - Maps docker service --mount option.
     default: []
   secrets:
     required: false
     description:
-    - List of dictionaries describing the service secrets.
-    - Every item must be a dictionary exposing the keys secret_id, secret_name, filename, uid (defaults to 0), gid (defaults to 0), mode (defaults to 0o444)
-    - Maps docker service --secret option.
+      - List of dictionaries describing the service secrets.
+      - Every item must be a dictionary exposing the keys secret_id, secret_name, filename, uid (defaults to 0), gid (defaults to 0), mode (defaults to 0o444)
+      - Maps docker service --secret option.
     default: []
   configs:
     required: false
     description:
-    - List of dictionaries describing the service configs.
-    - Every item must be a dictionary exposing the keys config_id, config_name, filename, uid (defaults to 0), gid (defaults to 0), mode (defaults to 0o444)
-    - Maps docker service --config option.
+      - List of dictionaries describing the service configs.
+      - Every item must be a dictionary exposing the keys config_id, config_name, filename, uid (defaults to 0), gid (defaults to 0), mode (defaults to 0o444)
+      - Maps docker service --config option.
     default: null
   networks:
     required: false
     default: []
     description:
-    - List of the service networks names.
-    - Maps docker service --network option.
+      - List of the service networks names.
+      - Maps docker service --network option.
   publish:
     default: []
     required: false
     description:
-    - List of dictionaries describing the service published ports.
-    - Every item must be a dictionary exposing the keys published_port, target_port, protocol (defaults to 'tcp')
-    - Only used with api_version >= 1.25
-    - If api_version >= 1.32 and docker python library >= 3.0.0 attribute 'mode' can be set to 'ingress' or 'host' (default 'ingress').
+      - List of dictionaries describing the service published ports.
+      - Every item must be a dictionary exposing the keys published_port, target_port, protocol (defaults to 'tcp')
+      - Only used with api_version >= 1.25
+      - If api_version >= 1.32 and docker python library >= 3.0.0 attribute 'mode' can be set to 'ingress' or 'host' (default 'ingress').
   replicas:
     required: false
     default: -1
     description:
-    - Number of containers instantiated in the service. Valid only if ``mode=='replicated'``.
-    - If set to -1, and service is not present, service replicas will be set to 1.
-    - If set to -1, and service is present, service replicas will be unchanged.
-    - Maps docker service --replicas option.
+      - Number of containers instantiated in the service. Valid only if ``mode=='replicated'``.
+      - If set to -1, and service is not present, service replicas will be set to 1.
+      - If set to -1, and service is present, service replicas will be unchanged.
+      - Maps docker service --replicas option.
   restart_policy:
     required: false
     default: none
     description:
-    - Restart condition of the service.
-    - Maps docker service --restart-condition option.
+      - Restart condition of the service.
+      - Maps docker service --restart-condition option.
     choices:
-    - none
-    - on-failure
-    - any
+      - none
+      - on-failure
+      - any
   restart_policy_attempts:
     required: false
     default: 0
     description:
-    - Maximum number of service restarts.
-    - Maps docker service --restart-max-attempts option.
+      - Maximum number of service restarts.
+      - Maps docker service --restart-max-attempts option.
   restart_policy_delay:
     required: false
     default: 0
     description:
-    - Delay between restarts.
-    - Maps docker service --restart-delay option.
+      - Delay between restarts.
+      - Maps docker service --restart-delay option.
   restart_policy_window:
     required: false
     default: 0
     description:
-    - Restart policy evaluation window.
-    - Maps docker service --restart-window option.
+      - Restart policy evaluation window.
+      - Maps docker service --restart-window option.
   update_delay:
     required: false
     default: 10
     description:
-    - Rolling update delay
-    - Maps docker service --update-delay option
+      - Rolling update delay
+      - Maps docker service --update-delay option
   update_parallelism:
     required: false
     default: 1
     description:
-    - Rolling update parallelism
-    - Maps docker service --update-parallelism option
+      - Rolling update parallelism
+      - Maps docker service --update-parallelism option
   update_failure_action:
     required: false
     default: continue
     description:
-    - Action to take in case of container failure
-    - Maps to docker service --update-failure-action option
+      - Action to take in case of container failure
+      - Maps to docker service --update-failure-action option
     choices:
-    - continue
-    - pause
+      - continue
+      - pause
   update_monitor:
     required: false
     default: 5000000000
     description:
-    - Time to monitor updated tasks for failures, in nanoseconds.
-    - Maps to docker service --update-monitor option
+      - Time to monitor updated tasks for failures, in nanoseconds.
+      - Maps to docker service --update-monitor option
   update_max_failure_ratio:
     required: false
     default: 0.00
     description:
-    - Fraction of tasks that may fail during an update before the failure action is invoked
-    - Maps to docker service --update-max-failure-ratio
+      - Fraction of tasks that may fail during an update before the failure action is invoked
+      - Maps to docker service --update-max-failure-ratio
   update_order:
     required: false
     default: null
     description:
-    - Specifies the order of operations when rolling out an updated task.
-    - Maps to docker service --update-order
-    - Requires docker api version >= 1.29
+      - Specifies the order of operations when rolling out an updated task.
+      - Maps to docker service --update-order
+      - Requires docker api version >= 1.29
   user:
     required: false
     default: root
     description:
-    - username or UID.
-    - "If set to C(null) the image provided value (or the one already
-       set for the service) will be used"
+      - username or UID.
+      - "If set to C(null) the image provided value (or the one already
+         set for the service) will be used"
 extends_documentation_fragment:
-- docker
+  - docker
 requirements:
-- "docker-py >= 2.0"
-- "Please note that the L(docker-py,https://pypi.org/project/docker-py/) Python
-   module has been superseded by L(docker,https://pypi.org/project/docker/)
-   (see L(here,https://github.com/docker/docker-py/issues/1310) for details).
-   Version 2.1.0 or newer is only available with the C(docker) module."
+  - "docker-py >= 2.0"
+  - "Please note that the L(docker-py,https://pypi.org/project/docker-py/) Python
+     module has been superseded by L(docker,https://pypi.org/project/docker/)
+     (see L(here,https://github.com/docker/docker-py/issues/1310) for details).
+     Version 2.1.0 or newer is only available with the C(docker) module."
 '''
 
 RETURN = '''
@@ -353,108 +352,117 @@ rebuilt:
 '''
 
 EXAMPLES = '''
--   name: define myservice
-    docker_swarm_service:
-        name: myservice
-        image: "alpine"
-        args:
-        - "sleep"
-        - "3600"
-        mounts:
-        -   source: /tmp/
-            target: /remote_tmp/
-            type: bind
-        env:
-        -   "ENVVAR1=envvar1"
-        log_driver: fluentd
-        log_driver_options:
-          fluentd-address: "127.0.0.1:24224"
-          fluentd-async-connect: true
-          tag: "{{.Name}}/{{.ID}}"
-        restart_policy: any
-        restart_policy_attempts: 5
-        restart_policy_window: 30
-    register: dss_out1
--   name: change myservice.env
-    docker_swarm_service:
-        name: myservice
-        image: "alpine"
-        args:
-        -   "sleep"
-        -   "7200"
-        mounts:
-        -   source: /tmp/
-            target: /remote_tmp/
-            type: bind
-        env:
-        -   "ENVVAR1=envvar1"
-        restart_policy: any
-        restart_policy_attempts: 5
-        restart_policy_window: 30
-    register: dss_out2
--   name: test for changed myservice facts
-    fail:
-        msg: unchanged service
-    when: "{{ dss_out1 == dss_out2 }}"
--   name: change myservice.image
-    docker_swarm_service:
-        name: myservice
-        image: "alpine:edge"
-        args:
-        -   "sleep"
-        -   "7200"
-        mounts:
-        -   source: /tmp/
-            target: /remote_tmp/
-            type: bind
-        env:
-        -   "ENVVAR1=envvar1"
-        restart_policy: any
-        restart_policy_attempts: 5
-        restart_policy_window: 30
-    register: dss_out3
--   name: test for changed myservice facts
-    fail:
-        msg: unchanged service
-    when: "{{ dss_out2 == dss_out3 }}"
--   name: remove mount
-    docker_swarm_service:
-        name: myservice
-        image: "alpine:edge"
-        args:
-        -   "sleep"
-        -   "7200"
-        env:
-        -   "ENVVAR1=envvar1"
-        restart_policy: any
-        restart_policy_attempts: 5
-        restart_policy_window: 30
-    register: dss_out4
--   name: test for changed myservice facts
-    fail:
-        msg: unchanged service
-    when: "{{ dss_out3 == dss_out4 }}"
--   name: keep service as it is
-    docker_swarm_service:
-        name: myservice
-        image: "alpine:edge"
-        args:
-        -   "sleep"
-        -   "7200"
-        env:
-        -   "ENVVAR1=envvar1"
-        restart_policy: any
-        restart_policy_attempts: 5
-        restart_policy_window: 30
-    register: dss_out5
--   name: test for changed service facts
-    fail:
-        msg: changed service
-    when: "{{ dss_out5 != dss_out5 }}"
--   name: remove myservice
-    docker_swarm_service:
-        name: myservice
-        state: absent
+- name: define myservice
+  docker_swarm_service:
+    name: myservice
+    image: "alpine"
+    args:
+      - "sleep"
+      - "3600"
+    mounts:
+      - source: /tmp/
+        target: /remote_tmp/
+        type: bind
+    env:
+      - "ENVVAR1=envvar1"
+    log_driver: fluentd
+    log_driver_options:
+      fluentd-address: "127.0.0.1:24224"
+      fluentd-async-connect: true
+      tag: "{{.Name}}/{{.ID}}"
+    restart_policy: any
+    restart_policy_attempts: 5
+    restart_policy_window: 30
+  register: dss_out1
+
+- name: change myservice.env
+  docker_swarm_service:
+    name: myservice
+    image: "alpine"
+    args:
+      - "sleep"
+      - "7200"
+    mounts:
+      - source: /tmp/
+        target: /remote_tmp/
+        type: bind
+    env:
+      - "ENVVAR1=envvar1"
+    restart_policy: any
+    restart_policy_attempts: 5
+    restart_policy_window: 30
+  register: dss_out2
+
+- name: test for changed myservice facts
+  fail:
+    msg: unchanged service
+  when: "{{ dss_out1 == dss_out2 }}"
+
+- name: change myservice.image
+  docker_swarm_service:
+    name: myservice
+    image: "alpine:edge"
+    args:
+      - "sleep"
+      - "7200"
+    mounts:
+      - source: /tmp/
+        target: /remote_tmp/
+        type: bind
+    env:
+      - "ENVVAR1=envvar1"
+    restart_policy: any
+    restart_policy_attempts: 5
+    restart_policy_window: 30
+  register: dss_out3
+
+- name: test for changed myservice facts
+  fail:
+    msg: unchanged service
+  when: "{{ dss_out2 == dss_out3 }}"
+
+- name: remove mount
+  docker_swarm_service:
+    name: myservice
+    image: "alpine:edge"
+    args:
+      - "sleep"
+      - "7200"
+    env:
+      - "ENVVAR1=envvar1"
+    restart_policy: any
+    restart_policy_attempts: 5
+    restart_policy_window: 30
+  register: dss_out4
+  
+- name: test for changed myservice facts
+  fail:
+    msg: unchanged service
+  when: "{{ dss_out3 == dss_out4 }}"
+  
+- name: keep service as it is
+  docker_swarm_service:
+    name: myservice
+    image: "alpine:edge"
+    args:
+      - "sleep"
+      - "7200"
+    env:
+      - "ENVVAR1=envvar1"
+    restart_policy: any
+    restart_policy_attempts: 5
+    restart_policy_window: 30
+  register: dss_out5
+
+- name: test for changed service facts
+  fail:
+    msg: changed service
+  when: "{{ dss_out5 != dss_out5 }}"
+
+- name: remove myservice
+  docker_swarm_service:
+    name: myservice
+    state: absent
 '''
 
 import time
